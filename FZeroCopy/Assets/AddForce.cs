a1d1f7746984de2489a79525aaa449f8a3434c51
fbHolzbaum <blancofel22@gmail.com>
2015-12-19 17:55:50 +0100

    Created Project

---
+++ FZeroCopy/Assets/AddForce.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForce : MonoBehaviour {
+
+    public float speed = 10000;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = transform.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if(Input.GetAxis("Vertical")!=0)
+        {
+            rb.AddForce(transform.forward * speed);
+        }
+	}
+}
